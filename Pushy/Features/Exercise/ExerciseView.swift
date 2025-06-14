07e4f00c4c64d519dbf550142f0b8add8805e5c8
mutakin-san <mutakin.email@gmail.com>
2025-06-14 14:54:16 +0700

    Adjust the size of placeholder guide

--- Pushy/Features/Exercise/ExerciseView.swift
+++ Pushy/Features/Exercise/ExerciseView.swift
@@ -8,34 +8,34 @@
 import SwiftUI
 
 struct ExerciseView: View {
-//    @StateObject private var viewModel = ExerciseViewModel()
-    
+    //    @StateObject private var viewModel = ExerciseViewModel()
+
     var body: some View {
         ZStack(alignment: .top) {
-//            if let image = viewModel.renderedImage {
-//                Image(uiImage: image)
-//                    .resizable()
-//                    .scaledToFit()
-//                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-//                    .background(Color.black)
-//            } else {
-//                Color.black
-//                    .overlay(Text("Starting Camera...").foregroundColor(.white))
-//            }
+            //            if let image = viewModel.renderedImage {
+            //                Image(uiImage: image)
+            //                    .resizable()
+            //                    .scaledToFit()
+            //                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            //                    .background(Color.black)
+            //            } else {
+            //                Color.black
+            //                    .overlay(Text("Starting Camera...").foregroundColor(.white))
+            //            }
 
-//            VStack(spacing: 4) {
-//                Text(viewModel.actionLabel)
-//                    .font(.title)
-//                    .bold()
-//                    .foregroundColor(.white)
-//                Text(viewModel.confidenceLabel)
-//                    .font(.subheadline)
-//                    .foregroundColor(.gray)
-//            }
-//            .padding()
-//            .background(Color.black.opacity(0.6))
-//            .cornerRadius(12)
-            
+            //            VStack(spacing: 4) {
+            //                Text(viewModel.actionLabel)
+            //                    .font(.title)
+            //                    .bold()
+            //                    .foregroundColor(.white)
+            //                Text(viewModel.confidenceLabel)
+            //                    .font(.subheadline)
+            //                    .foregroundColor(.gray)
+            //            }
+            //            .padding()
+            //            .background(Color.black.opacity(0.6))
+            //            .cornerRadius(12)
+
             // Add gradient overlay
             LinearGradient(
                 gradient: Gradient(colors: [
@@ -44,13 +44,13 @@ struct ExerciseView: View {
                     Color.clear,
                     Color.clear,
                     Color.black.opacity(0.3),
-                    Color.black.opacity(0.7)
+                    Color.black.opacity(0.7),
                 ]),
                 startPoint: .top,
                 endPoint: .bottom
             )
             .ignoresSafeArea()
-            
+
             HStack {
                 Button(action: {
                 }) {
@@ -74,23 +74,29 @@ struct ExerciseView: View {
             }
             .padding(.horizontal, 16)
             .padding(.top, 16)
-            
-            VStack {
-                Image("PositionGuide")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .offset(x: 0, y: 100)
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(
-                                Color.clear,
-                                lineWidth: 3)
-                            .padding(.bottom, 100)
-                            .offset(x: 0, y: 100)
-                    )
+
+            GeometryReader { geo in
+                VStack {
+                    Spacer()
+                    Image("PositionGuide")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geo.size.width * 0.5, height: geo.size.height * 0.8)
+                        .padding(.horizontal, 16)
+                        .padding(.top, 16)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(
+                                    Color.green,
+                                    lineWidth: 3
+                                )
+                        )
+                }
+                .ignoresSafeArea()
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .init(horizontal: .center, vertical: .bottom))
+                
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+
         }
     }
 }
